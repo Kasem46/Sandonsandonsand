1daad80b2ba680a7c3df61c69d1b32520df48d1d
Archived2706 <TheArchivist2706@gmail.com>
2024-05-28 13:55:26 -0400

    kinda hitbox manageing
    gag, rember to GetCurrentAnimatorStateInfo

--- Sanderbrandonsand/Assets/Scripts/HitboxManager.cs
+++ Sanderbrandonsand/Assets/Scripts/HitboxManager.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class HitboxManager : MonoBehaviour
 {
-   private GameObject player;
+    //the player and its important things
+    private GameObject player;
+    private Animator playerAnimator;
+
+    //each of the hitbox sets
+    public GameObject idleHitboxes;
+    public GameObject jumpingHitboxes;
+    public GameObject fallingHitboxes;
+    public GameObject crouchingHitboxes;
+    public GameObject forwardMoveHitboxes;
+    public GameObject backwardMoveHitboxes;
 
     // Start is called before the first frame update
     void Awake()
     {
         player = this.transform.parent.gameObject;
+        playerAnimator = player.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        setValidHitboxes();
+    }
+
+    private void setValidHitboxes() { 
         
     }
 }
