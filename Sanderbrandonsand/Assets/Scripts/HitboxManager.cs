368539c6624ecbc5fa15b0cb191ceaac40a2b874
Kasem46 <gabrielher2678@gmail.com>
2024-06-06 13:16:30 -0400

    get invalidated
    matew

--- Sanderbrandonsand/Assets/Scripts/HitboxManager.cs
+++ Sanderbrandonsand/Assets/Scripts/HitboxManager.cs
@@ -55,7 +55,7 @@ public class HitboxManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        setValidHitboxes();
+        
     }
 
     private void disableAll()
