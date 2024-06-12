13461ff6bd3b15f3f7dda727fc67b99c84b617e1
Archived2706 <thearchivist2706@gmail.com>
2024-06-12 13:41:13 -0400

    terrorism
    gag

--- Sanderbrandonsand/Assets/Scripts/GameManager.cs
+++ Sanderbrandonsand/Assets/Scripts/GameManager.cs
@@ -9,6 +9,9 @@ public class GameManager : MonoBehaviour
     public PlayerController player1;
     public PlayerController player2;
 
+    public GameObject squrHealth;
+    public GameObject trungHealth;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +21,22 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        squrHealth.transform.localScale = new Vector3((float)player1.getHealth()/(50f/3f),0.5f,1);
+        squrHealth.transform.position = new Vector3(-5f +(100- (float)player1.getHealth())/(100f/3f), 3.75f, -2f);
+        trungHealth.transform.localScale = new Vector3((float)player2.getHealth() / (50f / 3f), 0.5f, 1);
+        trungHealth.transform.position = new Vector3(5f - (100 - (float)player2.getHealth()) / (100f / 3f), 3.75f, -2f);
+
+
         if (player1.getHealth() <= 0)
         {
             Debug.Log("player 2 wins");
-            SceneManager.LoadScene("Player1Win");
+            SceneManager.LoadScene("Player2Win");
         }
 
         if (player2.getHealth() <= 0)
         {
             Debug.Log("player 1 wins");
-            SceneManager.LoadScene("Player2Win");
+            SceneManager.LoadScene("Player1Win");
         }
     }
 }
