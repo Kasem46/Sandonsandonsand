9c68a08ee869dcf7c8ccd5bc9552b1ade4c40d3b
Kasem46 <gabrielher2678@gmail.com>
2024-06-12 13:00:18 -0400

    bame bamager
    batthew

---
+++ Sanderbrandonsand/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public PlayerController player1;
+    public PlayerController player2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player1.getHealth() >= 0)
+        {
+            Debug.Log("player 2 wins");
+        }
+
+        if (player2.getHealth() >= 0)
+        {
+            Debug.Log("player 1 wins");
+        }
+    }
+}
