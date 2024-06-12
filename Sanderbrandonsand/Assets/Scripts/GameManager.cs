55413e0c70870375f2feb804e5019aa4c30cf13f
Kasem46 <gabrielher2678@gmail.com>
2024-06-12 13:09:50 -0400

    trungle rises again

--- Sanderbrandonsand/Assets/Scripts/GameManager.cs
+++ Sanderbrandonsand/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -20,11 +21,13 @@ public class GameManager : MonoBehaviour
         if (player1.getHealth() <= 0)
         {
             Debug.Log("player 2 wins");
+            SceneManager.LoadScene("Player1Win");
         }
 
         if (player2.getHealth() <= 0)
         {
             Debug.Log("player 1 wins");
+            SceneManager.LoadScene("Player2Win");
         }
     }
 }
